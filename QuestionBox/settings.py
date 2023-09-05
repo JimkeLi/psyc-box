791cf9b9ad9b969d0b495e6ce3eaac5d77ea5e8b
Di Chen <dichen@ip-192-168-43-18.us-west-2.compute.internal>
2023-09-05 23:05:39 +0800

    9.5

--- QuestionBox/settings.py
+++ QuestionBox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$*2mlk&-n6t117l^9y$6m0^wn4!))wijg&=dbpym@w46-pwd2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
